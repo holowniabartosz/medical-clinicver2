300e7477ed9247eb0564850e0f0625488a4297ee
holowniabartosz <holownia.bartosz@gmail.com>
2024-03-18 16:59:39 +0100

    v10.1 w/ doctors locations + tests

--- src/main/java/com/bobi89/medicalclinic/model/entity/mapper/PatientMapper.java
+++ src/main/java/com/bobi89/medicalclinic/model/entity/mapper/PatientMapper.java
@@ -4,6 +4,7 @@ import com.bobi89.medicalclinic.model.entity.patient.Patient;
 import com.bobi89.medicalclinic.model.entity.patient.PatientDTO;
 import com.bobi89.medicalclinic.model.entity.patient.PatientDTOwithPassword;
 import org.mapstruct.Mapper;
+
 @Mapper(componentModel = "spring")
 public interface PatientMapper {
 
