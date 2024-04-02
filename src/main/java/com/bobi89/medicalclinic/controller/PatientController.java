54940d2ea93c11bd92aebb623ed5cd08238506c6
holowniabartosz <holownia.bartosz@gmail.com>
2024-04-02 16:10:50 +0200

    v13.0 with liquibase, Duration-out

--- src/main/java/com/bobi89/medicalclinic/controller/PatientController.java
+++ src/main/java/com/bobi89/medicalclinic/controller/PatientController.java
@@ -4,6 +4,12 @@ import com.bobi89.medicalclinic.model.entity.patient.ChangePasswordCommand;
 import com.bobi89.medicalclinic.model.entity.patient.PatientDTO;
 import com.bobi89.medicalclinic.model.entity.patient.PatientDTOwithPassword;
 import com.bobi89.medicalclinic.service.patient_service.PatientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -17,16 +23,43 @@ public class PatientController {
 
     private PatientService patientService;
 
+    @Operation(summary = "Get the list of all patients")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List returned",
+                    content = {@Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = PatientDTO.class)))
+                    })
+    })
     @GetMapping
     public List<PatientDTO> findAll() {
         return patientService.findAll();
     }
 
+    @Operation(summary = "Get a patient by its email address")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the patient",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PatientDTO.class))
+            }),
+            @ApiResponse(responseCode = "404", description = "Entity not found",
+                    content = @Content)
+    })
     @GetMapping("/{email}")
     public PatientDTO findByEmail(@PathVariable String email) {
         return patientService.findByEmail(email);
     }
 
+    @Operation(summary = "Add a patient")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Patient created",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PatientDTO.class))
+                    }),
+            @ApiResponse(responseCode = "400", description = "Entity with this email already exists",
+                    content = @Content),
+            @ApiResponse(responseCode = "400", description = "Bad request",
+                    content = @Content)
+    })
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public PatientDTO save(@RequestBody PatientDTOwithPassword patientDTOwithPassword) {
@@ -34,6 +67,13 @@ public class PatientController {
                 .save(patientDTOwithPassword);
     }
 
+    @Operation(summary = "Delete a patient by its email address")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Patient deleted",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Entity not found",
+                    content = @Content)
+    })
     @DeleteMapping("/{email}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public String deleteByEmail(@PathVariable String email) {
@@ -42,12 +82,30 @@ public class PatientController {
         return "Removed patient with e-mail: " + email;
     }
 
+    @Operation(summary = "Update a patient")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Patient updated",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PatientDTO.class))
+                    }),
+            @ApiResponse(responseCode = "404", description = "Entity not found",
+                    content = @Content)
+    })
     @PutMapping("/{email}")
     public PatientDTO update(@PathVariable String email, @RequestBody PatientDTO patientDTO) {
         patientService.update(email, patientDTO);
         return patientService.findByEmail(patientDTO.getEmail());
     }
 
+    @Operation(summary = "Update patient's password (find patient by email)")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Password updated",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ChangePasswordCommand.class))
+                    }),
+            @ApiResponse(responseCode = "404", description = "Entity not found",
+                    content = @Content)
+    })
     @PatchMapping("password/{email}")
     public ChangePasswordCommand updatePatientPassword(@PathVariable String email,
                                                        @RequestBody ChangePasswordCommand pass) {
